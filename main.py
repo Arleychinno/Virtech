995d69c9d29db3f99f12b64fcc41728a237b1b09
Arleychinno <arleyrf20@gmail.com>
2026-09-11 19:04:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ def password():
     characters = string.ascii_letters + string.digits + string.punctuation
     generated_password = ''.join(random.choice(characters) for i in range(12))
 
-    return f'<h1>Password Generator</h1><p>Password kamu: <b>{generated_password}</b></p>'
+    return f'<p>Password kamu: <b>{generated_password}</b></p>'
     
 @app.route("/coin_flip")
 def coin():
     result = random.choice(["Heads", "Tails"])
 
-    return f'<h1>Coin Flip 🪙</h1><p>Hasil: <b>{result}</b></p>'
+    return f'<p>Hasil: <b>{result}</b></p>'
 
 app.run(debug=True)
